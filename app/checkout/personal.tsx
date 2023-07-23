2d8d1823411f839a47dd9455b15dbb82841760eb
unknown <smartkelvin66@gmail.com>
2023-07-23 22:04:00 +0100

    update

--- app/checkout/personal.tsx
+++ app/checkout/personal.tsx
@@ -1,13 +1,31 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { Link, useRouter } from "expo-router";
-import { Button, Card, TextInput, useTheme } from "react-native-paper";
+import {
+  Button,
+  Card,
+  HelperText,
+  TextInput,
+  useTheme,
+} from "react-native-paper";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PersonalInfoSchema, PersonalInfo } from "../../src/schema/checkout";
+import ControlledInput from "../../src/components/ControlledInput";
 
 const PersonDetails = () => {
+  const { control, handleSubmit } = useForm<PersonalInfo>({
+    resolver: zodResolver(PersonalInfoSchema),
+  });
+
+  const { setPersonal } = useCheckoutContext();
+
   const router = useRouter();
   const theme = useTheme();
 
-  const nextpage = () => {
+  const nextPage = (data: PersonalInfo) => {
+    setPersonal(data);
+
     router.push("/checkout/delivery");
   };
 
@@ -19,27 +37,43 @@ const PersonDetails = () => {
         width: "100%",
         alignSelf: "center",
       }}
-      showsHorizontalScrollIndicator={false}
+      showsVerticalScrollIndicator={false}
     >
       <Card style={{ backgroundColor: theme.colors.background }}>
-        <Card.Title title="personal information" titleVariant="titleLarge" />
+        <Card.Title title="Personal information" titleVariant="titleLarge" />
         <Card.Content style={{ gap: 10 }}>
-          <TextInput
-            label="name"
-            placeholder="name"
-            style={{ backgroundColor: theme.colors.background }}
+          <ControlledInput
+            control={control}
+            name="name"
+            placeholder="Name"
+            label="Name"
           />
-          <TextInput
-            label="Email"
+
+          <ControlledInput
+            control={control}
+            name="email"
             placeholder="hey@gmail.com"
-            style={{ backgroundColor: theme.colors.background }}
+            label="Email"
+          />
+
+          <ControlledInput
+            control={control}
+            name="password"
+            label="Password"
+            // secureTextEntry
+          />
+          <ControlledInput
+            control={control}
+            name="confirmPassword"
+            label="ConfirmPassword"
+            // secureTextEntry
           />
         </Card.Content>
       </Card>
-
-      <Button mode="contained" onPress={nextpage}></Button>
+      <Button onPress={handleSubmit(nextPage)} mode="contained">
+        Next
+      </Button>
     </ScrollView>
   );
 };
-
 export default PersonDetails;
