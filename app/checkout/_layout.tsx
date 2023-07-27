62f9baca47fb153c530943b78795e8a6dbecb1ff
unknown <smartkelvin66@gmail.com>
2023-07-27 12:56:23 +0100

    form validation

--- app/checkout/_layout.tsx
+++ app/checkout/_layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useCheckoutContext } from "../../src/contexts/CheckoutContext";
 
 const CheckoutStack = () => {
   return (
